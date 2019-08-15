96b5392482e7a05f852a6f799eb48d9766ccbc72
iamkucuk <furkankucuk@outlook.com>
2019-08-16 01:19:50 +0300

    further changes for evaluation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageDraw
 from matplotlib import pyplot as plt
 from scipy.spatial.distance import cosine
 #%%
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cpu")#'cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Running on device: {}'.format(device))
 #%%
 mtcnn = MTCNN(device=device)
@@ -19,11 +19,18 @@ resnet = InceptionResnetV1(pretrained='casia-webface').eval().to(device)
 #%%
 img = Image.open("dataset/matthew2.jpg")
 img_cropped1 = mtcnn(img)
+plt.imshow(img_cropped1.permute((1, 2, 0)))
 #%%
 img = Image.open("dataset/matthew3.jpg")
 img_cropped2 = mtcnn(img)
+plt.imshow(img_cropped2.permute((1, 2, 0)))
 #%%
 img_embedding1 = resnet(img_cropped1.unsqueeze(0).to(device)).cpu().detach().numpy()
 img_embedding2 = resnet(img_cropped2.unsqueeze(0).to(device)).cpu().detach().numpy()
 
 dist = cosine(img_embedding1, img_embedding2)
+
+#%%
+dist
+
+#%%
